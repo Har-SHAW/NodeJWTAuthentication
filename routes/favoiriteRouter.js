8b2c6d9fea8a1b96909b43819c10133e51aa0eb5
Har-SHAW <8888harsha4444@gmail.com>
2020-07-23 21:03:14 +0530

    final

--- routes/favoiriteRouter.js
+++ routes/favoiriteRouter.js
@@ -1,299 +1,145 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const Favourites = require("../models/favourite");
-const dishRouter = express.Router();
-var authenticate = require('../authenticate');
+const favouriteRouter = express.Router();
+var authenticate = require("../authenticate");
 const Fourites = require("../models/favourite");
 
-dishRouter.use(bodyParser.json());
+favouriteRouter.use(bodyParser.json());
 
-dishRouter.route('/')
-.get((req,res,next) => {
-    Favourites.find({})
-    .populate('dishes')
-    .populate('user')
-    .then((dishes) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(dishes);
-    }, (err) => next(err))
-    .catch((err) => next(err));
-})
+favouriteRouter
+  .route("/")
+  .get(authenticate.verifyUser, (req, res, next) => {
+    Favourites.findOne({ user: req.user._id })
+      .populate("dishes")
+      .populate("user")
+      .then(
+        (favs) => {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(favs);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
+  })
   .post(authenticate.verifyUser, (req, res, next) => {
-      Fourites.findOne({user: req.user._id}).then((fav) => {
-          if(fav !== null){
-              
-              fav.dishes.push(req.body);
-              
-              fav.save().then((dish) => {
-                console.log("Dish Created ", dish);
-                res.statusCode = 200;
-                res.setHeader("Content-Type", "application/json");
-                res.json(dish);
-              }, (err) => next(err));
-          }else{
-              Favourites.create({user: req.user._id}).then((fav) => {
-                
-                  fav.dishes.push(req.body);
-                  
-                  fav.save().then((dish) => {
-                    console.log("Dish Created ", dish);
-                    res.statusCode = 200;
-                    res.setHeader("Content-Type", "application/json");
-                    res.json(dish);
-                  }, (err) => next(err));
-              })
+    Fourites.findOne({ user: req.user._id })
+      .then((fav) => {
+        if (fav !== null) {
+          var favs = req.body;
+          for (var i = 0; i < favs.length; i++) {
+            if (fav.dishes.indexOf(favs[i]._id) === -1)
+              fav.dishes.push(favs[i]._id);
           }
-      })
-      .catch((err) => next(err));
-  })
-  .put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-    res.statusCode = 403;
-    res.end("PUT operation not supported on /dishes");
-  })
-  .delete(authenticate.verifyUser,authenticate.verifyAdmin, (req, res, next) => {
-    Dishes.remove({})
-      .then(
-        (resp) => {
-          res.statusCode = 200;
-          res.setHeader("Content-Type", "application/json");
-          res.json(resp);
-        },
-        (err) => next(err)
-      )
-      .catch((err) => next(err));
-  });
-
-  dishRouter.route('/:dishId')
-  .get((req,res,next) => {
-      Dishes.findById(req.params.dishId)
-      .populate('comments.author')
-      .then((dish) => {
-          res.statusCode = 200;
-          res.setHeader('Content-Type', 'application/json');
-          res.json(dish);
-      }, (err) => next(err))
-      .catch((err) => next(err));
-  })
-  .post(authenticate.verifyUser, authenticate.verifyAdmin,  (req, res, next) => {
-    res.statusCode = 403;
-    res.end("POST operation not supported on /dishes/" + req.params.dishId);
-  })
-  .put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-    Dishes.findByIdAndUpdate(
-      req.params.dishId,
-      {
-        $set: req.body,
-      },
-      { new: true }
-    )
-      .then(
-        (dish) => {
-          res.statusCode = 200;
-          res.setHeader("Content-Type", "application/json");
-          res.json(dish);
-        },
-        (err) => next(err)
-      )
-      .catch((err) => next(err));
-  })
-  .delete(authenticate.verifyUser,authenticate.verifyAdmin, (req, res, next) => {
-    Dishes.findByIdAndRemove(req.params.dishId)
-      .then(
-        (resp) => {
-          res.statusCode = 200;
-          res.setHeader("Content-Type", "application/json");
-          res.json(resp);
-        },
-        (err) => next(err)
-      )
-      .catch((err) => next(err));
-  });
-
-  dishRouter.route('/:dishId/comments')
-  .get((req,res,next) => {
-      Dishes.findById(req.params.dishId)
-      .populate('comments.author')
-      .then((dish) => {
-          if (dish != null) {
+          fav.save().then(
+            (f) => {
+              console.log("Fav Created ", f);
               res.statusCode = 200;
-              res.setHeader('Content-Type', 'application/json');
-              res.json(dish.comments);
-          }
-          else {
-              err = new Error('Dish ' + req.params.dishId + ' not found');
-              err.status = 404;
-              return next(err);
-          }
-      }, (err) => next(err))
-      .catch((err) => next(err));
-  })
-  .post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-      Dishes.findById(req.params.dishId)
-      .then((dish) => {
-          if (dish != null) {
-              req.body.author = req.user._id;
-              dish.comments.push(req.body);
-              dish.save()
-              .then((dish) => {
-                  Dishes.findById(dish._id)
-                  .populate('comments.author')
-                  .then((dish) => {
-                      res.statusCode = 200;
-                      res.setHeader('Content-Type', 'application/json');
-                      res.json(dish);
-                  })            
-              }, (err) => next(err));
-          }
-          else {
-              err = new Error('Dish ' + req.params.dishId + ' not found');
-              err.status = 404;
-              return next(err);
-          }
-      }, (err) => next(err))
-      .catch((err) => next(err));
-  })
-  .put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-    res.statusCode = 403;
-    res.end(
-      "PUT operation not supported on /dishes/" +
-        req.params.dishId +
-        "/comments"
-    );
-  })
-  .delete(authenticate.verifyUser, authenticate.verifyAdmin,  (req, res, next) => {
-    Dishes.findById(req.params.dishId)
-      .then(
-        (dish) => {
-          if (dish != null) {
-            for (var i = dish.comments.length - 1; i >= 0; i--) {
-              dish.comments.id(dish.comments[i]._id).remove();
+              res.setHeader("Content-Type", "application/json");
+              res.json(f);
+            },
+            (err) => next(err)
+          );
+        } else {
+          Favourites.create({ user: req.user._id }).then((fav) => {
+            var favs = req.body;
+            for (var i = 0; i < favs.length; i++) {
+              fav.dishes.push(favs[i]._id);
             }
-            dish.save().then(
-              (dish) => {
+            fav.save().then(
+              (f) => {
+                console.log("fav Created ", f);
                 res.statusCode = 200;
                 res.setHeader("Content-Type", "application/json");
-                res.json(dish);
+                res.json(f);
               },
               (err) => next(err)
             );
-          } else {
-            err = new Error("Dish " + req.params.dishId + " not found");
-            err.status = 404;
-            return next(err);
-          }
+          });
+        }
+      })
+      .catch((err) => next(err));
+  })
+  .put(authenticate.verifyUser, (req, res, next) => {
+    res.statusCode = 403;
+    res.end("PUT operation not supported on /dishes");
+  })
+  .delete(authenticate.verifyUser, (req, res, next) => {
+    Favourites.remove({ user: req.user._id })
+      .then(
+        (resp) => {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(resp);
         },
         (err) => next(err)
       )
       .catch((err) => next(err));
   });
 
-  dishRouter.route('/:dishId/comments/:commentId')
-  .get((req,res,next) => {
-      Dishes.findById(req.params.dishId)
-      .populate('comments.author')    
-      .then((dish) => {
-          if (dish != null && dish.comments.id(req.params.commentId) != null) {
-              res.statusCode = 200;
-              res.setHeader('Content-Type', 'application/json');
-              res.json(dish.comments.id(req.params.commentId));
-          }
-          else if (dish == null) {
-              err = new Error('Dish ' + req.params.dishId + ' not found');
-              err.status = 404;
-              return next(err);
-          }
-          else {
-              err = new Error('Comment ' + req.params.commentId + ' not found');
-              err.status = 404;
-              return next(err);            
-          }
-      }, (err) => next(err))
-      .catch((err) => next(err));
-  })
-  .post(authenticate.verifyUser,authenticate.verifyNotAdmin, (req, res, next) => {
+favouriteRouter
+  .route("/:dishId")
+  .get((req, res, next) => {
     res.statusCode = 403;
-    res.end(
-      "POST operation not supported on /dishes/" +
-        req.params.dishId +
-        "/comments/" +
-        req.params.commentId
-    );
+    res.end("GET operation not supported on /favourites/" + dishId);
   })
-  .put(authenticate.verifyUser, authenticate.verifyNotAdmin,(req, res, next) => {
-    Dishes.findById(req.params.dishId)
-    .then((dish) => {
-        if (dish != null && dish.comments.id(req.params.commentId) != null) {
-          if((req.user._id).equals(dish.comments.id(req.params.commentId).author)){
-            if (req.body.rating) {
-                dish.comments.id(req.params.commentId).rating = req.body.rating;
-            }
-            if (req.body.comment) {
-                dish.comments.id(req.params.commentId).comment = req.body.comment;                
-            }
-            dish.save()
-            .then((dish) => {
-                Dishes.findById(dish._id)
-                .populate('comments.author')
-                .then((dish) => {
-                    res.statusCode = 200;
-                    res.setHeader('Content-Type', 'application/json');
-                    res.json(dish);  
-                })              
-            }, (err) => next(err));
-          }else{
-            err = new Error('Your are not allowed to do this operation');
-            err.status = 404;
-            return next(err);
-          }
+  .post(authenticate.verifyUser, (req, res, next) => {
+    Fourites.findOne({ user: req.user._id }).then((fav) => {
+      if (fav !== null) {
+        if (fav.dishes.indexOf(req.params.dishId) === -1) {
+          fav.dishes.push(req.params.dishId)
+          fav
+            .save()
+            .then(
+              (fav) => {
+                res.statusCode = 200;
+                res.setHeader("Content-Type", "application/json");
+                res.json(fav);
+              },
+              (err) => next(err)
+            )
+            .catch((err) => next(err));
+        } else {
+          res.statusCode = 500;
+          res.end("Dish already exists in Favourite dishes");
         }
-        else if (dish == null) {
-            err = new Error('Dish ' + req.params.dishId + ' not found');
-            err.status = 404;
-            return next(err);
-        }
-        else {
-            err = new Error('Comment ' + req.params.commentId + ' not found');
-            err.status = 404;
-            return next(err);            
-        }
-    }, (err) => next(err))
-    .catch((err) => next(err));
-})
-.delete(authenticate.verifyUser,authenticate.verifyNotAdmin,  (req, res, next) => {
-    Dishes.findById(req.params.dishId)
-    .then((dish) => {
-        if (dish != null && dish.comments.id(req.params.commentId) != null) {
-          if((req.user._id).equals(dish.comments.id(req.params.commentId).author)){
-            dish.comments.id(req.params.commentId).remove();
-            dish.save()
-            .then((dish) => {
-                Dishes.findById(dish._id)
-                .populate('comments.author')
-                .then((dish) => {
-                    res.statusCode = 200;
-                    res.setHeader('Content-Type', 'application/json');
-                    res.json(dish);  
-                })               
-            }, (err) => next(err));
-          }else{
-            err = new Error('you are not allowed to do this');
-            err.status = 404;
-            return next(err);
-          }
-        }
-        else if (dish == null) {
-            err = new Error('Dish ' + req.params.dishId + ' not found');
-            err.status = 404;
-            return next(err);
-        }
-        else {
-            err = new Error('Comment ' + req.params.commentId + ' not found');
-            err.status = 404;
-            return next(err);            
-        }
-    }, (err) => next(err))
-    .catch((err) => next(err));
-});
+      } else {
+        Favourites.create({ user: req.user._id }).then((fav) => {
+          fav.dishes.push(req.params.dishId);
+          fav
+            .save()
+            .then(
+              (f) => {
+                res.statusCode = 200;
+                res.setHeader("Content-Type", "application/json");
+                res.json(f);
+              },
+              (err) => next(err)
+            )
+            .catch((err) => next(err));
+        });
+      }
+    });
+  })
+  .put(authenticate.verifyUser, (req, res, next) => {
+    res.statusCode = 403;
+    res.end("PUT operation not supported on /favourites");
+  })
+  .delete(authenticate.verifyUser, (req, res, next) => {
+    Favourites.findOne({ user: req.user._id })
+      .then((fav) => {
+        fav.dishes.remove(req.params.dishId);
+        fav.save().then(
+          (f) => {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            res.json(f);
+          },
+          (err) => next(err)
+        );
+      })
+      .catch((err) => next(err));
+  });
 
-module.exports = dishRouter;
+module.exports = favouriteRouter;
